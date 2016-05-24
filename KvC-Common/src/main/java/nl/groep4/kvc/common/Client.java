b2e6aad3c6d3a9477e59497873310d56e785f3f3
Tim Biesenbeek <makertim@outlook.com>
2016-05-24 12:29:22 +0200

    FIX: Javadoc of Client Interface

--- KvC-Common/src/main/java/nl/groep4/kvc/common/Client.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/Client.java
@@ -8,27 +8,27 @@ public interface Client extends Remote, Serializable {
 
 	/**
 	 * @param msg
-	 *            Stuur een log naar client op Level#FINE
+	 *            De message die in een log naar client op Level#FINE
 	 * @throws RemoteException
-	 *             mocht er een remote probleem zijn, throw die
+	 *             mocht er een remote probleem zijn
 	 * @see java.util.logging.Level
 	 */
 	public void logFine(String msg) throws RemoteException;
 
 	/**
 	 * @param msg
-	 *            Stuur een log naar client op Level#INFO
+	 *            De message die in een log naar client op Level#INFO
 	 * @throws RemoteException
-	 *             mocht er een remote probleem zijn, throw die
+	 *             mocht er een remote probleem zijn
 	 * @see java.util.logging.Level
 	 */
 	public void logInfo(String msg) throws RemoteException;
 
 	/**
 	 * @param msg
-	 *            Stuur een log naar client op Level#WARNING
+	 *            De message die in een log naar client op Level#WARNING
 	 * @throws RemoteException
-	 *             mocht er een remote probleem zijn, throw die
+	 *             mocht er een remote probleem zijn
 	 * @see java.util.logging.Level
 	 */
 	public void logWarn(String msg) throws RemoteException;
