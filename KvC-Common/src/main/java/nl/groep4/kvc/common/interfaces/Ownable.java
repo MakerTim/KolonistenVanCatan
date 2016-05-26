39bfada6ea3f8361a07dad66ec3b4ad9b0386797
MakerTim <MakerTim@outlook.com>
2016-05-26 09:18:16 +0200

    FIX: Nullable is not a mvn supported class

--- KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/Ownable.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/Ownable.java
@@ -1,12 +1,9 @@
 package nl.groep4.kvc.common.interfaces;
 
-import com.sun.istack.internal.Nullable;
-
 import nl.groep4.kvc.common.Player;
 
 public interface Ownable {
 
-	@Nullable
 	public Player getOwner();
 
 	public void setOwner(Player player);
--- KvC-Common/src/main/java/nl/groep4/kvc/common/map/Tile.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/map/Tile.java
@@ -1,7 +1,5 @@
 package nl.groep4.kvc.common.map;
 
-import com.sun.istack.internal.Nullable;
-
 import nl.groep4.kvc.common.enumeration.Direction;
 import nl.groep4.kvc.common.enumeration.Point;
 
@@ -9,13 +7,10 @@ public interface Tile {
 
 	public String getPosition();
 
-	@Nullable
 	public Tile getRelativeTile(Direction direction);
 
-	@Nullable
 	public Street getStreet(Direction direction);
 
-	@Nullable
 	public Building getBuilding(Point point);
 
 }
