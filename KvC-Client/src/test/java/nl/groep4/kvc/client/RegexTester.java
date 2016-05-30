0195876723d78f96e47c21f49160243507258c4b
Bachir <s1089585@student.hsleiden.nl>
2016-05-30 14:51:54 +0200

    ADDED: RegexTester.java

--- KvC-Client/src/test/java/nl/groep4/kvc/client/RegexTester.java
+++ KvC-Client/src/test/java/nl/groep4/kvc/client/RegexTester.java
@@ -1,6 +1,6 @@
 package nl.groep4.kvc.client;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -11,6 +11,25 @@ public class RegexTester {
     @Test
     public void ipRegex() {
 	// TODO: TEST -> KvCStatcis.REGEX_IP
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("192.101.130.111".matches(KvCStatcis.REGEX_IP));
+	assertTrue("1.1.1.1".matches(KvCStatcis.REGEX_IP));
+	assertTrue("20.20.20.20".matches(KvCStatcis.REGEX_IP));
+	assertTrue("150.15.1.5".matches(KvCStatcis.REGEX_IP));
+	// assertTrue("Localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue(!" . . . ".matches(KvCStatcis.REGEX_IP));
+	assertTrue("....".matches(KvCStatcis.REGEX_IP));
+	assertTrue("1000.1000.1000.1000".matches(KvCStatcis.REGEX_IP));
+	assertTrue("google.com".matches(KvCStatcis.REGEX_IP));
+	assertTrue("www.google.com".matches(KvCStatcis.REGEX_IP));
+	assertTrue("----".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
+	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
 	assertTrue("localhost".matches(KvCStatcis.REGEX_IP));
     }
 
