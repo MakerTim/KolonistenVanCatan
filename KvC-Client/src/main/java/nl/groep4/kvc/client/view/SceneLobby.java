7fc823ed358a48ccccce4fff435e4124d1e94247
luc runge <lucrunge2@gmail.com>
2016-05-31 15:13:46 +0200

    Added: Lobby menu (just a basic screen with no updates)

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/SceneLobby.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/SceneLobby.java
@@ -1,21 +1,45 @@
 package nl.groep4.kvc.client.view;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import nl.groep4.kvc.client.util.SceneUtil;
+import nl.groep4.kvc.client.view.elements.LobbyButton;
 import nl.groep4.kvc.common.Lobby;
 
 /**
  * 
  * 
  * @version 1.0
- * @author Tim
+ * @author Luc
  */
 
 public class SceneLobby implements SceneHolder {
 
     @Override
     public Scene getScene() {
-	// TODO: SoundUtil.stopTeamsong();
-	return null;
+	/* Build multiple layers for the design */
+	Pane lobbyPane = new Pane();
+
+	/* Build the settings menu in lobby */
+	Text lobby = new Text(838, 200, "Lobby");
+	lobby.setFont(ViewMaster.FONT);
+	lobby.setFill(Color.WHITE);
+	lobby.setStroke(Color.BLACK);
+	LobbyButton startGame = new LobbyButton(415, 550, "Start Game");
+	startGame.setFont(ViewMaster.FONT);
+
+	lobbyPane.getChildren().addAll(SceneUtil.getMenuBackground(), SceneUtil.getLobbyForeground(),
+		SceneUtil.getLobbyBrazier(), SceneUtil.getCornerShield(), lobby, startGame);
+
+	Scene scene = new Scene(lobbyPane);
+	SceneUtil.fadeIn(SceneUtil.getLobbySettings());
+
+	return scene;
+
+	// TODO: SoundUtil.stopTeamsong(); wanneer spel start
+
     }
 
     public void update(Lobby lobby) {
