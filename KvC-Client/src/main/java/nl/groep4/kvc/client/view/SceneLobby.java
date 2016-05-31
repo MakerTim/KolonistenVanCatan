be1df5deafeba4f051375abf6ed1dc2116aeaa5d
Tim Biesenbeek <makertim@outlook.com>
2016-05-31 14:44:56 +0200

    FIX: Login / Lobby naming struggles

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/SceneLobby.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/SceneLobby.java
@@ -1,6 +1,7 @@
 package nl.groep4.kvc.client.view;
 
 import javafx.scene.Scene;
+import nl.groep4.kvc.common.Lobby;
 
 public class SceneLobby implements SceneHolder {
 
@@ -8,6 +9,10 @@ public class SceneLobby implements SceneHolder {
     public Scene getScene() {
 	// TODO: SoundUtil.stopTeamsong();
 	return null;
+    }
+
+    public void update(Lobby lobby) {
+	// TODO: Everytime there is a update - then this method gets called
     }
 
 }
