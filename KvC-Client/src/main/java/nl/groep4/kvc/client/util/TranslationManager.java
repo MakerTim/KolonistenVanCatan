47df2dfeacd5ce3a65e18ea50cf4d1d013109e4e
Bachir <s1089585@student.hsleiden.nl>
2016-06-02 12:05:12 +0200

    CHANGED: setLanguage

--- KvC-Client/src/main/java/nl/groep4/kvc/client/util/TranslationManager.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/util/TranslationManager.java
@@ -1,13 +1,41 @@
 package nl.groep4.kvc.client.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import jdk.nashorn.api.scripting.URLReader;
+
 public class TranslationManager {
 
+    public static void main(String[] args) {
+	setLanguage("en-EN");
+
+    }
+
     public static void setLanguage(String languageKey) {
+	try {
+	    BufferedReader br = new BufferedReader(
+		    new URLReader(TranslationManager.class.getResource("/lang/" + languageKey + ".yml")));
+	    try {
+		String x;
+		while ((x = br.readLine()) != null) {
+		    // printing out each line in the file
+		    System.out.println(x);
+		}
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	} catch (Exception e) {
+	    System.out.println(e);
+	    e.printStackTrace();
+	}
 
     }
 
     public static String translate(String key, Object... args) {
-	return String.format(key, args);
+
+	return key;
+
     }
 
 }
