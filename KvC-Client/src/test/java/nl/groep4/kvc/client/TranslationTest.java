1e7b9ab8f94cd83699cc548f13263838003b4b1a
Bachir <s1089585@student.hsleiden.nl>
2016-06-02 15:17:45 +0200

    FIX: Translation Test

--- KvC-Client/src/test/java/nl/groep4/kvc/client/TranslationTest.java
+++ KvC-Client/src/test/java/nl/groep4/kvc/client/TranslationTest.java
@@ -1,11 +1,11 @@
 package nl.groep4.kvc.client;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import nl.groep4.kvc.client.util.TranslationManager;
-
-import static org.junit.Assert.*;
 
 public class TranslationTest {
 
@@ -17,7 +17,6 @@ public class TranslationTest {
     @Test
     public void translateTest() {
 	assertEquals("Translating the name of the language", "English", TranslationManager.translate("language"));
-	assertEquals("Random translation ", "English", TranslationManager.translate("language"));
     }
 
     @Test
@@ -29,7 +28,7 @@ public class TranslationTest {
     @Test
     public void translateWithArgs() {
 	for (int i = 0; i < 1000; i++) {
-	    assertEquals("Args", "Round: " + i, TranslationManager.translate("game.main.round", i));
+	    assertEquals("Args", "Round: " + i, TranslationManager.translate("game.name.round", i));
 	}
     }
 
