7ab35bf2c3c6448e37263892960a4bf3276e2385
matthijs <s1094397@student.hsleiden.nl>
2016-06-07 11:11:17 +0200

    Added: Javadoc ThrowController

--- KvC-Client/src/main/java/nl/groep4/kvc/client/controller/ThrowController.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/controller/ThrowController.java
@@ -1,7 +1,16 @@
 package nl.groep4.kvc.client.controller;
 
+/**
+ * controller when throwing dice
+ * 
+ * @author Tim
+ * @version 1.0
+ */
 public class ThrowController {
 
+    /**
+     * throws dice
+     */
     public static void throwDice() {
 	// TODO: ThrowController#throwDice
     }
