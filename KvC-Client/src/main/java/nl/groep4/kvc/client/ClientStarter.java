622d56675a5cd9dcad6130f13822bdfc3842cc69
Tim Biesenbeek <makertim@outlook.com>
2016-06-09 14:23:33 +0200

    Invalid commit

--- KvC-Client/src/main/java/nl/groep4/kvc/client/ClientStarter.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/ClientStarter.java
@@ -2,6 +2,7 @@ package nl.groep4.kvc.client;
 
 import javafx.application.Application;
 import nl.groep4.kvc.client.view.ViewMaster;
+import nl.groep4.kvc.client.view.scene.SceneMap;
 
 /**
  * Starts the client
@@ -18,6 +19,14 @@ public class ClientStarter {
      *            contains the supplied command-line arguments
      */
     public static void main(String[] args) {
+	new Thread(() -> {
+	    try {
+		Thread.sleep(10000);
+	    } catch (Exception ex) {
+	    }
+	    System.out.println("scenemap");
+	    ViewMaster.setScene(new SceneMap());
+	}).start();
 	Application.launch(ViewMaster.class, args);
     }
 
