5266ff93e80252803dfb2852618dd30fe723355e
MakerTim <MakerTim@outlook.com>
2016-06-13 23:51:38 +0200

    Fixed nullpointering

--- KvC-Client/src/main/java/nl/groep4/kvc/client/debug/TestMapViewSetup.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/debug/TestMapViewSetup.java
@@ -5,7 +5,6 @@ import java.rmi.RemoteException;
 import nl.groep4.kvc.client.controller.ClientRefrence;
 import nl.groep4.kvc.client.view.ViewMaster;
 import nl.groep4.kvc.client.view.scene.SceneMap;
-import nl.groep4.kvc.common.map.Coordinate;
 
 public class TestMapViewSetup {
 
@@ -14,7 +13,6 @@ public class TestMapViewSetup {
 	ViewMaster.setScene(view);
 	TestMapController controller = new TestMapController(view);
 	controller.getMap().createMap();
-	controller.getMap().getStreet(new Coordinate(0.5, 0)).setOwner(controller.getPlayer());
 	ClientRefrence.setThePlayer(controller.getPlayer());
 	view.registerController(controller);
 	try {
