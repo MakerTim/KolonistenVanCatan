61e9410c5c3abb7db59e4ae0d90379bdb58fe798
luc runge <lucrunge2@gmail.com>
2016-06-13 14:47:11 +0200

    Added: base collection pane

---
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/CollectionPane.java
@@ -0,0 +1,19 @@
+package nl.groep4.kvc.client.view.pane;
+
+import javafx.scene.layout.Pane;
+
+public class CollectionPane implements PaneHolder {
+
+    @Override
+    public Pane getPane() {
+	// TODO Auto-generated method stub
+	return new Pane();
+    }
+
+    @Override
+    public void updateTranslation() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
