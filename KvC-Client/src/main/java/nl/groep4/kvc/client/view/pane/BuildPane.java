93b1d5ff3745e97d69bb0794849e98522407d049
MakerTim <MakerTim@outlook.com>
2016-06-13 20:50:24 +0200

    bachir > FIX: 2 panes

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/BuildPane.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/BuildPane.java
@@ -1,40 +1,81 @@
 package nl.groep4.kvc.client.view.pane;
 
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.geometry.Pos;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import nl.groep4.kvc.client.util.SceneUtil;
+import nl.groep4.kvc.client.util.TranslationManager;
+import nl.groep4.kvc.client.view.ViewMaster;
+import nl.groep4.kvc.client.view.elements.MenuButton;
 
-@SuppressWarnings("unused")
-public class BuildPane extends Application implements PaneHolder {
+public class BuildPane implements PaneHolder {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-	StackPane Build = new StackPane();
-	Build.setMinSize(50, 50);
-
-	Label title = new Label();
-
-	Button Button1 = new Button("Build");
-	Group root = new Group();
-
-	Build.getChildren().add(Button1);
-	root.getChildren().add(Build);
-
-	primaryStage.show();
-
-    }
+    MenuButton road = new MenuButton(425, 500, TranslationManager.translate("build.button.road"));
+    MenuButton village = new MenuButton(425, 500, TranslationManager.translate("build.button.village"));
+    MenuButton city = new MenuButton(425, 500, TranslationManager.translate("build.button.city"));
 
     @Override
     public Pane getPane() {
-	return null;
+	// TODO Auto-generated method stub
+
+	MenuButton road = new MenuButton(425, 500, TranslationManager.translate("build.button.road"));
+	MenuButton village = new MenuButton(425, 500, TranslationManager.translate("build.button.village"));
+	MenuButton city = new MenuButton(425, 500, TranslationManager.translate("build.button.city"));
+
+	VBox vbox = new VBox(8);
+	vbox.setPrefWidth(300);
+	vbox.setPrefHeight(450);
+	vbox.setAlignment(Pos.CENTER);
+
+	StackPane Build = new StackPane();
+	Build.setAlignment(Pos.CENTER);
+	Build.setMinSize(1366, 768);
+	Text Empty2 = new Text("");
+	Text Empty = new Text("");
+	Text Empty3 = new Text("");
+	Text Empty4 = new Text("");
+
+	Text Prices = new Text("   Materials.row:  \t\t\t Wht \t wod \t Wol \t Sto \t Ore");
+	Text RoadPrices = new Text("Road.row:\t\t\t\t 0 \t\t 1 \t\t 0 \t\t 1 \t\t 0");
+	Text VillagePrices = new Text("Village.row:\t\t\t 1 \t\t 1 \t\t 1 \t\t 1 \t\t 1");
+	Text CityPrices = new Text("City.row:\t\t\t\t 3 \t\t 0 \t\t 0 \t\t 0 \t\t 2");
+
+	Font BuildFont = new Font("Impact", 14);
+	Prices.setFont(BuildFont);
+	RoadPrices.setFont(BuildFont);
+	RoadPrices.setFill(Color.WHITE);
+	RoadPrices.setStroke(Color.BLACK);
+	VillagePrices.setFont(BuildFont);
+	VillagePrices.setFill(Color.WHITE);
+	VillagePrices.setStroke(Color.BLACK);
+	CityPrices.setFont(BuildFont);
+	CityPrices.setFill(Color.WHITE);
+	CityPrices.setStroke(Color.BLACK);
+	Prices.setFill(Color.WHITE);
+	Prices.setStroke(Color.BLACK);
+	road.setFont(ViewMaster.FONT);
+	village.setFont(ViewMaster.FONT);
+	city.setFont(ViewMaster.FONT);
+
+	vbox.getChildren().addAll(Prices, RoadPrices, VillagePrices, CityPrices, Empty, Empty2, Empty3, Empty4, road,
+		village, city);
+	Build.getChildren().addAll(SceneUtil.getBuildMenuBackground(), vbox);
+
+	return Build;
+
     }
 
     @Override
     public void updateTranslation() {
+	// TODO Auto-generated method stub
+
+	road.updateText(TranslationManager.translate("build.button.road"));
+	village.updateText(TranslationManager.translate("build.button.village"));
+	city.updateText(TranslationManager.translate("build.button.city"));
 
     }
 }
