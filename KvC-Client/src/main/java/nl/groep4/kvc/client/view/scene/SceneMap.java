0cdb8b59a9b01d7be557ca04c070f4b752c62f3e
MakerTim <MakerTim@outlook.com>
2016-06-13 23:52:43 +0200

    FIX: Unused import

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/scene/SceneMap.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/scene/SceneMap.java
@@ -5,7 +5,6 @@ import java.util.EnumMap;
 import java.util.List;
 
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
