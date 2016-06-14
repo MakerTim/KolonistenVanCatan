78a7d67b979e5bbc3cd192babc4bc520f60ee2be
matthijs <s1094397@student.hsleiden.nl>
2016-06-14 16:13:25 +0200

    Edited: TradePane layout

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/TradePane.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/TradePane.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
@@ -56,11 +57,35 @@ public class TradePane extends Application implements PaneHolder, UpdateTrade {
 	gp.setVgap(10);
 	gp.setPadding(new Insets(50, 50, 50, 50));
 
-	Text category = new Text("Wheat");
-	category.setFill(Color.WHITE);
-	category.setStroke(Color.BLACK);
-	category.setFont(ViewMaster.FONT);
-	gp.add(category, 1, 0);
+	Text wheat = new Text("Wheat");
+	wheat.setFill(Color.WHITE);
+	wheat.setStroke(Color.BLACK);
+	wheat.setFont(ViewMaster.FONT);
+	gp.add(wheat, 14, 0);
+
+	Text wood = new Text("Wood");
+	wood.setFill(Color.WHITE);
+	wood.setStroke(Color.BLACK);
+	wood.setFont(ViewMaster.FONT);
+	gp.add(wood, 16, 0);
+
+	Text wool = new Text("Wool");
+	wool.setFill(Color.WHITE);
+	wool.setStroke(Color.BLACK);
+	wool.setFont(ViewMaster.FONT);
+	gp.add(wool, 18, 0);
+
+	Text stone = new Text("Stone");
+	stone.setFill(Color.WHITE);
+	stone.setStroke(Color.BLACK);
+	stone.setFont(ViewMaster.FONT);
+	gp.add(stone, 20, 0);
+
+	Text ore = new Text("Ore");
+	ore.setFill(Color.WHITE);
+	ore.setStroke(Color.BLACK);
+	ore.setFont(ViewMaster.FONT);
+	gp.add(ore, 22, 0);
 
 	ScrollPane scrollPane = new ScrollPane(hbox);
 	scrollPane.getStylesheets().add("/assets/stylesheet.css");
@@ -72,17 +97,18 @@ public class TradePane extends Application implements PaneHolder, UpdateTrade {
 	BorderPane bp = new BorderPane();
 
 	bp.getChildren().addAll(SceneUtil.getGamePane(), gp, root);
-	root.setPadding(new Insets(60));
+	root.setPadding(new Insets(80, 60, 60, 60));
 
 	scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
 	scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 	scrollPane.setMinHeight(280);
 	scrollPane.setMinWidth(575);
 
+	buttons.setAlignment(Pos.BOTTOM_RIGHT);
 	buttons.getChildren().addAll(terug, plaats);
 	root.setBottom(buttons);
 
-	Scene scene = new Scene(bp, 800, 400);
+	Scene scene = new Scene(bp, 800, 600);
 
 	primaryStage.setScene(scene);
 	primaryStage.show();
