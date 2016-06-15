db7305b2a620e40d4eaa38ac183371c86ae405e9
matthijs <s1094397@student.hsleiden.nl>
2016-06-15 10:26:52 +0200

    Fixed: TradePane

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/TradePane.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/TradePane.java
@@ -3,11 +3,9 @@ package nl.groep4.kvc.client.view.pane;
 import java.rmi.RemoteException;
 import java.util.List;
 
-import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.BorderPane;
@@ -18,14 +16,13 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import nl.groep4.kvc.client.util.SceneUtil;
 import nl.groep4.kvc.client.view.ViewMaster;
 import nl.groep4.kvc.client.view.elements.MenuButton;
 import nl.groep4.kvc.common.interfaces.Trade;
 import nl.groep4.kvc.common.interfaces.UpdateTrade;
 
-public class TradePane extends Application implements PaneHolder, UpdateTrade {
+public class TradePane implements PaneHolder, UpdateTrade {
     StackPane pane = new StackPane();
     Group root = new Group();
     MenuButton plaats = new MenuButton("Plaats");
@@ -37,18 +34,6 @@ public class TradePane extends Application implements PaneHolder, UpdateTrade {
 
     @Override
     public Pane getPane() {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    @Override
-    public void updateTranslation() {
-	// TODO Auto-generated method stub
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
 	HBox hbox = new HBox();
 	HBox buttons = new HBox();
 	HBox tb = new HBox();
@@ -107,15 +92,12 @@ public class TradePane extends Application implements PaneHolder, UpdateTrade {
 	buttons.setAlignment(Pos.BOTTOM_RIGHT);
 	buttons.getChildren().addAll(terug, plaats);
 	root.setBottom(buttons);
-
-	Scene scene = new Scene(bp, 800, 600);
-
-	primaryStage.setScene(scene);
-	primaryStage.show();
+	return bp;
     }
 
-    public static void main(String[] args) {
-	launch(args);
+    @Override
+    public void updateTranslation() {
+	// TODO Auto-generated method stub
     }
 
     @Override
