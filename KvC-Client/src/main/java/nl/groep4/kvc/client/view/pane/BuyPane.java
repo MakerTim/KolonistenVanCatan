ca74097f3b8b5ff085c187d58784185bb510680d
Tim Biesenbeek <makertim@outlook.com>
2016-06-16 14:46:08 +0200

    Added BuyPane

---
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/BuyPane.java
@@ -0,0 +1,17 @@
+package nl.groep4.kvc.client.view.pane;
+
+import javafx.scene.layout.Pane;
+
+public class BuyPane implements PaneHolder {
+
+    @Override
+    public Pane getPane() {
+	return new Pane();
+    }
+
+    @Override
+    public void updateTranslation() {
+
+    }
+
+}
