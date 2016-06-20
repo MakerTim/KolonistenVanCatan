22ed37f3885299e036d33579dd3a0139e8e008a6
luc runge <lucrunge2@gmail.com>
2016-06-20 10:40:20 +0200

    TIM WIL DE COMMIT ZIEN

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/ScorePane.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/ScorePane.java
@@ -1,14 +1,26 @@
 package nl.groep4.kvc.client.view.pane;
 
+import java.rmi.RemoteException;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map.Entry;
+
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import nl.groep4.kvc.client.view.ViewMaster;
+import nl.groep4.kvc.common.enumeration.Resource;
+import nl.groep4.kvc.common.interfaces.Card;
+import nl.groep4.kvc.common.interfaces.Player;
+import nl.groep4.kvc.common.interfaces.UpdatePlayerOrder;
+import nl.groep4.kvc.common.interfaces.UpdateStock;
 
-public class ScorePane implements PaneHolder {
+public class ScorePane implements PaneHolder, UpdateStock, UpdatePlayerOrder {
 
     StackPane scorePane;
     VBox players;
@@ -16,15 +28,38 @@ public class ScorePane implements PaneHolder {
     ImageView banner;
     ImageView closedBanner;
     Text title;
+    Text firstSlot;
+    Text secondSlot;
+    Text thirdSlot;
+    Text fourthSlot;
+    Text fifthSlot;
+    Text sixthSlot;
 
     @Override
     public Pane getPane() {
 	scorePane = new StackPane();
 	players = new VBox();
 	content = new VBox();
-	title = new Text();
+	title = new Text("Title");
+	firstSlot = new Text("player 1\n1 0 1 2 0");
+	firstSlot.setFill(Color.WHITE);
+	secondSlot = new Text("player 2\n1 0 1 2 0");
+	secondSlot.setFill(Color.WHITE);
+	thirdSlot = new Text("player 3\n1 0 1 2 0");
+	thirdSlot.setFill(Color.WHITE);
+	fourthSlot = new Text("player 4\n1 0 1 2 0");
+	fourthSlot.setFill(Color.WHITE);
+	fifthSlot = new Text("player 5\n1 0 1 2 0");
+	fifthSlot.setFill(Color.WHITE);
+	sixthSlot = new Text("player 6\n1 0 1 2 0");
+	sixthSlot.setFill(Color.WHITE);
+	title.setFont(ViewMaster.FONT);
+	title.setFill(Color.WHITE);
+	title.setStroke(Color.BLACK);
 
-	content.getChildren().addAll(title, players);
+	content.getChildren().addAll(title, players, firstSlot, secondSlot, thirdSlot, fourthSlot, fifthSlot,
+		sixthSlot);
+	content.setAlignment(Pos.TOP_CENTER);
 
 	scorePane.getChildren().addAll(getClosedBanner());
 
@@ -61,6 +96,38 @@ public class ScorePane implements PaneHolder {
     @Override
     public void updateTranslation() {
 	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updateStock(Player pl, EnumMap<Resource, Integer> resources) throws RemoteException {
+	for (Entry<Resource, Integer> resource : resources.entrySet()) {
+	    Resource res = resource.getKey();
+	    String amnt = resource.getValue().toString();
+	    switch (res) {
+	    case BRICK:
+		break;
+	    case ORE:
+		break;
+	    case WHEAT:
+		break;
+	    case WOOD:
+		break;
+	    case WOOL:
+		break;
+	    }
+
+	}
+    }
+
+    @Override
+    public void updateStock(Player pl, List<Card> cards) throws RemoteException {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updatePlayerOrder(List<Player> order) throws RemoteException {
 
     }
 }
