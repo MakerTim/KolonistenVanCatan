b7186fe9c44df477204e9069b87ba72449c41915
Lisa <s1098175@student.hsleiden.nl>
2016-06-21 17:00:30 +0200

    ADDED: inventionpane and monopolypane

---
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/InventionPane.java
@@ -0,0 +1,19 @@
+package nl.groep4.kvc.client.view.pane;
+
+import javafx.scene.layout.Pane;
+
+public class InventionPane implements PaneHolder {
+
+    @Override
+    public Pane getPane() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public void updateTranslation() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
