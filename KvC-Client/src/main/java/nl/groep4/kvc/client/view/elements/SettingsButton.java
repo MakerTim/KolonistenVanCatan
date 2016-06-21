a9206668ebaced2826ae262ed5d88c9ec6d8ca75
matthijs <s1094397@student.hsleiden.nl>
2016-06-21 21:54:37 +0200

    Added: Javadoc to SettingsButton

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/elements/SettingsButton.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/elements/SettingsButton.java
@@ -6,12 +6,21 @@ import nl.groep4.kvc.client.view.ViewMaster;
 import nl.groep4.kvc.client.view.scene.SceneHolder;
 import nl.groep4.kvc.client.view.scene.SceneSettings;
 
+/**
+ * Button linked to settings menu. Updates when settings are changed.
+ * 
+ * @author Tim
+ * @version 1.0
+ */
 public class SettingsButton extends MenuButton {
 
     private static final SettingsButton BUTTON = new SettingsButton();
 
     private SceneHolder parent;
 
+    /**
+     * Translates button and activates when registers a click
+     */
     public SettingsButton() {
 	super(TranslationManager.translate("lobby.button.settings"));
 	setFont(ViewMaster.FONT);
@@ -28,10 +37,24 @@ public class SettingsButton extends MenuButton {
 	}
     }
 
+    /**
+     * Translates button string
+     */
     public static void updateConfig() {
 	BUTTON.updateText(TranslationManager.translate("lobby.button.settings"));
     }
 
+    /**
+     * gets button settings
+     * 
+     * @param parent
+     *            parent of button
+     * @param x
+     *            sets the value of the property layoutX
+     * @param y
+     *            sets the value of the property layoutY
+     * @return
+     */
     public static SettingsButton getButton(SceneHolder parent, int x, int y) {
 	BUTTON.parent = parent;
 	BUTTON.setLayoutX(x);
