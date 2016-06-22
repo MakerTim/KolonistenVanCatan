68ded5dfd16e3b80cf25ee0f1e39c85927de4ffc
matthijs <s1094397@student.hsleiden.nl>
2016-06-22 14:13:32 +0200

    Added: Javadoc to UpdatablePane and UpdateDice

--- KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/UpdateDice.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/UpdateDice.java
@@ -2,8 +2,24 @@ package nl.groep4.kvc.common.interfaces;
 
 import java.rmi.RemoteException;
 
+/**
+ * Updates dice
+ * 
+ * @author Tim
+ * @version 1.0
+ */
 public interface UpdateDice {
 
+    /**
+     * Updates dice1 and dice2
+     * 
+     * @param dice1
+     *            left dice value
+     * @param dice2
+     *            right dice value
+     * @throws RemoteException
+     *             signals when exception occurs
+     */
     public void updateDices(int dice1, int dice2) throws RemoteException;
 
 }
