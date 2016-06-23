f6cfc02470d66c9abb8c92e7dddf21a10c6055e4
Lisa <s1098175@student.hsleiden.nl>
2016-06-23 14:36:17 +0200

    added: javadoc

--- KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/Trade.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/Trade.java
@@ -7,38 +7,38 @@ import java.util.UUID;
 import nl.groep4.kvc.common.enumeration.Resource;
 
 /**
- * Interface of everything a Trade can do
+ * Interface of everything a Trade can do.
  * 
  * @author Tim
- * @version 1.0
+ * @version 1.1
  */
 public interface Trade extends Serializable {
 
     /**
-     * Gets a unique ID
+     * Gets a unique ID.
      * 
-     * @return a unique ID
+     * @return A unique ID.
      */
     public UUID getID();
 
     /**
-     * Gets the player who wants to trade
+     * Gets the player who wants to trade.
      * 
-     * @return the player who wants to trade
+     * @return The player who wants to trade.
      */
     public Player getPlayer();
 
     /**
-     * Gets a map of the request of the trade
+     * Gets a map of the request of the trade.
      * 
-     * @return the request of the trade
+     * @return The request of the trade.
      */
     public EnumMap<Resource, Integer> getRequest();
 
     /**
-     * Gets the map of the reward of the trade
+     * Gets the map of the reward of the trade.
      * 
-     * @return the reward of the trade
+     * @return The reward of the trade.
      */
     public EnumMap<Resource, Integer> getReward();
 
