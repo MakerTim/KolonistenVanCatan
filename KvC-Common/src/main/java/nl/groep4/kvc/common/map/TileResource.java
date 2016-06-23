b869e540409ce924a92005b8158595acdf934d7a
matthijs <s1094397@student.hsleiden.nl>
2016-06-23 14:31:23 +0200

    Added: Javadoc to TileResource, TileSea and TileType
    [ci skip]

--- KvC-Common/src/main/java/nl/groep4/kvc/common/map/TileResource.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/map/TileResource.java
@@ -2,9 +2,25 @@ package nl.groep4.kvc.common.map;
 
 import nl.groep4.kvc.common.enumeration.Resource;
 
+/**
+ * Gets resource and number.
+ * 
+ * @author Tim
+ * @version 1.0
+ */
 public interface TileResource extends TileLand {
 
+    /**
+     * Gets resource.
+     * 
+     * @return Type of resource.
+     */
     public Resource getResource();
 
+    /**
+     * Gets number.
+     * 
+     * @return Number.
+     */
     public int getNumber();
 }
