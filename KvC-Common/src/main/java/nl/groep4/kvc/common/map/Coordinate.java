6b0b7d1ea3c339b945f1bd7058abbd26d687ab68
Lisa <s1098175@student.hsleiden.nl>
2016-06-23 16:01:37 +0200

    added: translation

--- KvC-Common/src/main/java/nl/groep4/kvc/common/map/Coordinate.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/map/Coordinate.java
@@ -3,10 +3,10 @@ package nl.groep4.kvc.common.map;
 import java.io.Serializable;
 
 /**
- * 
+ * Its the reference to the location.
  * 
  * @author Tim
- * @version 1.0
+ * @version 1.1
  */
 public final class Coordinate implements Serializable, Cloneable {
 
@@ -15,23 +15,59 @@ public final class Coordinate implements Serializable, Cloneable {
     private double x;
     private double y;
 
+    /**
+     * Sets up the coordinates.
+     * 
+     * @param x
+     *            The xCoordinate.
+     * @param y
+     *            The yCoordinate.
+     */
     public Coordinate(double x, double y) {
 	this.x = x;
 	this.y = y;
     }
 
+    /**
+     * Gets the key of the item you want the coordinate from.
+     * 
+     * @return The key.
+     */
     public String getKey() {
 	return String.format("%d;%d", x, y);
     }
 
+    /**
+     * Returns true or false on if item is in the same location.
+     * 
+     * @param x
+     *            The xCoordinate
+     * @param y
+     *            The yCoordinate
+     * @return True or false on if item is in the same location.
+     */
     public boolean isSameLocation(double x, double y) {
 	return this.x == x && this.y == y;
     }
 
+    /**
+     * Adds coordinates to an item.
+     * 
+     * @param added
+     *            The added coordinates.
+     * @return The item with the added coordinates.
+     */
     public Coordinate add(Coordinate added) {
 	return add(added.getX(), added.getY());
     }
 
+    /**
+     * Multiplies the coordinates.
+     * 
+     * @param multiplied
+     *            The coordinate that needs to be multiplied.
+     * @return The multiplied coordinate
+     */
     public Coordinate multiply(Coordinate multiplied) {
 	return multiply(multiplied.getX(), multiplied.getY());
     }
