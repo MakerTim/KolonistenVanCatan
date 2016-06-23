901a0aee381047997a1ebd7c3e380be8d00029da
matthijs <s1094397@student.hsleiden.nl>
2016-06-23 11:26:25 +0200

    Fixed: Errors Javadoc in Player
    [ci skip]

--- KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/UpdatePlayerOrder.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/UpdatePlayerOrder.java
@@ -3,8 +3,22 @@ package nl.groep4.kvc.common.interfaces;
 import java.rmi.RemoteException;
 import java.util.List;
 
+/**
+ * Updates in what order the players are displayed
+ * 
+ * @author Tim
+ * @version 1.0
+ */
 public interface UpdatePlayerOrder {
 
+    /**
+     * Updates the player list
+     * 
+     * @param order
+     *            the sequence players are in the list
+     * @throws RemoteException
+     *             any remotely invoked method
+     */
     public void updatePlayerOrder(List<Player> order) throws RemoteException;
 
 }
