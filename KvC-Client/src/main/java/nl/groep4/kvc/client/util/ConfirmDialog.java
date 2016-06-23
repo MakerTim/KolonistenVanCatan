9dfbcbcf534088e7db7ce7df383244f8c3304bc4
Tim Biesenbeek <makertim@outlook.com>
2016-06-23 13:31:46 +0200

    FIX: Confirm always on

--- KvC-Client/src/main/java/nl/groep4/kvc/client/util/ConfirmDialog.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/util/ConfirmDialog.java
@@ -4,18 +4,22 @@ import java.util.Optional;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import nl.groep4.kvc.client.controller.ClientRefrence;
 import javafx.scene.control.ButtonType;
 
 public class ConfirmDialog {
 
     public static boolean confirm(String key) {
-	Alert alert = new Alert(AlertType.CONFIRMATION);
-	alert.setTitle(TranslationManager.translate("dialog.title"));
-	alert.setHeaderText(TranslationManager.translate("dialog." + key + ".desc"));
-	alert.setContentText(TranslationManager.translate("dialog.confirm"));
+	if (ClientRefrence.isConfirmModus()) {
+	    Alert alert = new Alert(AlertType.CONFIRMATION);
+	    alert.setTitle(TranslationManager.translate("dialog.title"));
+	    alert.setHeaderText(TranslationManager.translate("dialog." + key + ".desc"));
+	    alert.setContentText(TranslationManager.translate("dialog.confirm"));
 
-	Optional<ButtonType> result = alert.showAndWait();
-	return result.get() == ButtonType.OK;
+	    Optional<ButtonType> result = alert.showAndWait();
+	    return result.get() == ButtonType.OK;
+	}
+	return true;
     }
 
 }
