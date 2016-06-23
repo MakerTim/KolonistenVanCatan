4b22d92a4765197372568fb387987b045263d159
matthijs <s1094397@student.hsleiden.nl>
2016-06-23 14:25:36 +0200

    Added: Javadoc to TileLand
    [ci skip]

--- KvC-Common/src/main/java/nl/groep4/kvc/common/map/TileLand.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/map/TileLand.java
@@ -1,10 +1,27 @@
 package nl.groep4.kvc.common.map;
 
+/**
+ * Checks if rover is on tile, places rover and removes rover.
+ * 
+ * @author Tim
+ * @version 1.0
+ */
 public interface TileLand extends Tile {
 
+    /**
+     * Checks If rover is on tile.
+     * 
+     * @return True or false, depends if rover is on the selected tile.
+     */
     public boolean hasRover();
 
+    /**
+     * Paces rover.
+     */
     public void placeRover();
 
+    /**
+     * Removes rover.
+     */
     public void removeRover();
 }
