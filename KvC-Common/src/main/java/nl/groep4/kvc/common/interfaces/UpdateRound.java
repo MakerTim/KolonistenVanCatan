d30121e4a2183167fe983a3155f58e8e00017baa
Lisa <s1098175@student.hsleiden.nl>
2016-06-23 11:37:49 +0200

    added: javadoc

--- KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/UpdateRound.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/interfaces/UpdateRound.java
@@ -4,9 +4,34 @@ import java.rmi.RemoteException;
 
 import nl.groep4.kvc.common.enumeration.TurnState;
 
+/**
+ * Skeleton fot the UpdateRound
+ * 
+ * @author Tim
+ * @version
+ */
 public interface UpdateRound {
 
+    /**
+     * Updates the round in the game
+     * 
+     * @param round
+     *            number of the round
+     * @throws RemoteException
+     *             any remotely invoked method
+     */
     public void updateRound(int round) throws RemoteException;
 
+    /**
+     * Updates the turn in the game. When a player ends his turn the next player
+     * is in line, and the game has to update that
+     * 
+     * @param who
+     *            the player whose turn it is
+     * @param what
+     *            the state of the player, is it his turn or not
+     * @throws RemoteException
+     *             any remotely invoked method
+     */
     public void updateTurn(Player who, TurnState what) throws RemoteException;
 }
