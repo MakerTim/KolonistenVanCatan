56871f9290e70ec0071a1a7ea85465f53ccc46ec
Bachir <s1089585@student.hsleiden.nl>
2016-06-27 15:15:58 +0200

    ADDED javadoc to KvCStatics
    [ci skip]

--- KvC-Common/src/main/java/nl/groep4/kvc/common/KvCStatics.java
+++ KvC-Common/src/main/java/nl/groep4/kvc/common/KvCStatics.java
@@ -1,20 +1,33 @@
 package nl.groep4.kvc.common;
 
+/**
+ * This class shows the input that is being used in the lobby.
+ * 
+ * @author Tim version: 1.0
+ *
+ */
 public class KvCStatics {
     /**
-     * Name of lobby stored in LOBBY_KEY to connect with
+     * 
+     * 
      */
     public static final String LOBBY_KEY = "KvCLobby";
 
     /**
-     * REGEX: ipv4 adress or a domain name (with sub) or a dynamic name
+     * REGEX: ipv4 address or a domain name (with sub) or a dynamic name.
      */
     public static final String REGEX_IP = "((?:[0-9]{1,3}\\.){3}[0-9]{1,3})|(([a-zA-Z0-9|-]+\\.)*[a-zA-Z0-9|-]+\\.[a-zA-Z]+)|(([a-zA-Z\\-])+)";
 
-    /** REGEX: only numbers */
+    /**
+     * REGEX: only numbers
+     * 
+     */
     public static final String NUMERIC = "[0-9]";
 
-    /** REGEX: only characters, capital charcaters or numbers */
+    /**
+     * REGEX: only characters, capital characters or numbers.
+     * 
+     */
     public static final String USERNAME = "[a-zA-Z0-9]";
 
 }
