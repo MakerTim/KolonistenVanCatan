874e7162c49ae71ea3edbedeb1e030d715b4eeba
Bachir <s1089585@student.hsleiden.nl>
2016-06-27 14:29:57 +0200

    ADDED javadoc to ServerVictoryCard
    [ci skip]

--- KvC-Server/src/main/java/nl/groep4/kvc/server/model/ServerVictoryCard.java
+++ KvC-Server/src/main/java/nl/groep4/kvc/server/model/ServerVictoryCard.java
@@ -6,9 +6,26 @@ import nl.groep4.kvc.common.interfaces.VictoryCard;
 
 public class ServerVictoryCard extends ServerCard implements VictoryCard {
 
+    /**
+     * The serverside victorcard is created here
+     * 
+     * version: 1.0
+     * 
+     * @author: Tim
+     * 
+     */
     private static final long serialVersionUID = 666L;
 
     private VictoryCardType subType;
+
+    /**
+     * The constructor for the serverside victorycard
+     * 
+     * @param type
+     *            The type of cards in main cards.
+     * @param subType
+     *            The type of cards in subcards
+     */
 
     public ServerVictoryCard(CardType type, VictoryCardType subType) {
 	super(type);
