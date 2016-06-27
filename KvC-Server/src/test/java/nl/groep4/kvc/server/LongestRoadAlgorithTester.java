b1eb75d9bbfad9d1018b79b7df87400602944374
Bachir <s1089585@student.hsleiden.nl>
2016-06-27 15:09:23 +0200

    FIXED LongestRoadAlgorithTester ( Deleted all javadoc)
    [ci skip]

--- KvC-Server/src/test/java/nl/groep4/kvc/server/LongestRoadAlgorithTester.java
+++ KvC-Server/src/test/java/nl/groep4/kvc/server/LongestRoadAlgorithTester.java
@@ -16,12 +16,6 @@ import nl.groep4.kvc.server.model.ServerPlayer;
 import nl.groep4.kvc.server.model.map.ServerMap;
 import nl.groep4.kvc.server.util.RoadFinder;
 
-/**
- * 
- * @author Tim
- *
- */=
-
 public class LongestRoadAlgorithTester {
 
     private Map map;
