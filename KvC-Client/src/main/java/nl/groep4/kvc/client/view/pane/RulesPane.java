27873f59730e8c7b42ecddf467b7730f0b4e2218
Lisa <s1098175@student.hsleiden.nl>
2016-06-28 17:33:21 +0200

    fixed: rulepane with a scrollpane

--- KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/RulesPane.java
+++ KvC-Client/src/main/java/nl/groep4/kvc/client/view/pane/RulesPane.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import jdk.nashorn.api.scripting.URLReader;
 import nl.groep4.kvc.client.util.SceneUtil;
 import nl.groep4.kvc.client.util.TranslationManager;
@@ -23,12 +26,13 @@ import nl.groep4.kvc.client.view.scene.SceneMap;
  * Pane for the rules of the game.
  * 
  * @author Lisa
- * @version 1.1
+ * @version 1.2
  */
 public class RulesPane implements PaneHolder {
     private KvCText rules = new KvCText(TranslationManager.translate("rules.text.rules"));
     private SceneMap view;
-    private MenuButton back;
+    private MenuButton back = new MenuButton(425, 500, TranslationManager.translate("rules.button.back"));
+    // private Font font = new Font(ViewMaster.FONT.getName(), 17);
 
     /**
      * Sets up the Rulespane.
@@ -44,31 +48,26 @@ public class RulesPane implements PaneHolder {
     @Override
     public Pane getPane() {
 	StackPane rulepane = new StackPane();
-
-	TextArea area = new TextArea();
+	ScrollPane scrollpane = new ScrollPane();
 
 	VBox vbox = new VBox();
+	VBox lines = new VBox();
 	HBox hbox = new HBox();
+
+	back.setFont(ViewMaster.FONT);
+
+	scrollpane.setHbarPolicy(ScrollBarPolicy.NEVER);
+	scrollpane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+	scrollpane.setMinWidth(500);
+	scrollpane.setMaxWidth(600);
+	scrollpane.setMinHeight(300);
+	scrollpane.setMaxHeight(360);
+	scrollpane.setContent(lines);
 
 	vbox.setAlignment(Pos.CENTER);
 	vbox.setPadding(new Insets(40, 80, 40, 80));
-
-	back = new MenuButton(425, 500, TranslationManager.translate("rules.button.back"));
-	back.setFont(ViewMaster.FONT);
-
-	area.setStyle("-fx-text-fill: white;");
-	area.setPrefRowCount(18);
-	area.setMaxWidth(600);
-	area.setEditable(false);
-	area.setOnKeyPressed(key -> key.consume());
-
-	Node background = SceneUtil.getGamePane();
-
+	lines.setAlignment(Pos.CENTER_LEFT);
 	hbox.setAlignment(Pos.CENTER);
-
-	hbox.getChildren().add(back);
-	vbox.getChildren().addAll(rules, area, hbox);
-	rulepane.getChildren().addAll(background, vbox);
 
 	BufferedReader fileReader = new BufferedReader(new URLReader(TranslationManager.class
 		.getResource("/assets/Spelregels-" + TranslationManager.getCurrentLanguage() + ".txt")));
@@ -76,13 +75,21 @@ public class RulesPane implements PaneHolder {
 	    Scanner scanner = new Scanner(fileReader);
 	    while (scanner.hasNextLine()) {
 		String line = scanner.nextLine() + "\n";
-		area.appendText(line);
+		Text text = new Text(line);
+		text.setFill(Color.WHITE);
+		lines.getChildren().add(text);
+
 	    }
 	    scanner.close();
 	} catch (Exception e) {
 	    e.getStackTrace();
 	}
 
+	Node background = SceneUtil.getGamePane();
+
+	hbox.getChildren().add(back);
+	vbox.getChildren().addAll(rules, scrollpane, hbox);
+	rulepane.getChildren().addAll(background, vbox);
 	rulepane.getStylesheets().add("/assets/stylesheet.css");
 
 	back.registerClick(() -> onBackClick());
